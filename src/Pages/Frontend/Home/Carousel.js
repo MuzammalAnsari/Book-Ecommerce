83f82540240245709f7fb89487178c39cc55ecc5
MuzammalAnsari <muzammalbash@gmail.com>
2024-02-02 10:38:44 +0500

    i removed the button on the carousel

--- src/Pages/Frontend/Home/Carousel.js
+++ src/Pages/Frontend/Home/Carousel.js
@@ -52,9 +52,9 @@ export default function Carousel({ slides }) {
                     );
                 })}
             </div>
-            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500">
+            {/* <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-red-500">
                 <Button text="Browse Store" />
-            </div>
+            </div> */}
         </div>
     );
 }
